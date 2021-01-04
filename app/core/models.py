85eff8d288398676756e995ab74f916bc1475872
조현지 <huji3156@gmail.com>
2021-01-05 00:35:09 +0900

    Add validation for email field, Add test of normalizing email

--- app/core/models.py
+++ app/core/models.py
@@ -5,9 +5,20 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password=None, **extra_fields)
-    user = self.model(email=email, **extra_fields)
-    user.set_password(password)
-    user.save(using=self._db)
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
-    return user
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,3 +15,9 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         # password 는 암호화되기 때문에 check_password() 를 사용해야함
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = 'test@GOOGLE.COM'
+        user = get_user_model().objects.create_user(email, 'Testpass1234')
+
+        self.assertEqual(user.email, email.lower())
